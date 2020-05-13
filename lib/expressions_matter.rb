faa028a282a63fc9de66041aefbaa0c80489c5dc
plkujaw <jawor.jakub@gmail.com>
2020-05-13 11:44:12 +0000

    refactored

--- lib/expressions_matter.rb
+++ lib/expressions_matter.rb
@@ -1,14 +1,9 @@
 def expression_matter(a, b, c)
   results = []
-  r1 = a * (b + c)
-  results << r1
-  r2 = a * b * c
-  results << r2
-  r3 = a + b * c
-  results << r3
-  r4 = (a + b) * c
-  results << r4
-  r5 = a + b + c
-  results << r5
+  results << a * (b + c)
+  results << a * b * c
+  results << a + b * c
+  results << (a + b) * c
+  results << a + b + c
   results.max
 end
